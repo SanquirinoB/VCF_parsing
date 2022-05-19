e660d04823cd94a75bee2ecb29fea94217508981
sanquirinob <fernanda.sanchirico@ug.uchile.cl>
2022-05-19 00:21:13 -0400

    Upsi

--- mean_length_contig.py
+++ mean_length_contig.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
         line = VCF.readline()
 
         while line[:2] == "##":
-
+            
             if line[:9] == "##contig=": # line = "##contig=<ID=GL000224.1,assembly=b37,length=179693>\n"
 
                 for x in line[10:-1].split(","): # line[10:-1] = "ID=GL000224.1,assembly=b37,length=179693"  
@@ -16,5 +16,6 @@ if __name__ == "__main__":
                     if pair[0] == "length": # Necessary for invertion calculus
                         length += int(pair[1])
                         n += 1
+            line = VCF.readline()
 
     print("Max pos over reference is arround", length / n, ".")
