aef6184d2ad1bfa9695ec94efc63827bc1bd1aac
sanquirinob <fernanda.sanchirico@ug.uchile.cl>
2022-05-19 00:18:11 -0400

    More functions

---
+++ mean_length_contig.py
@@ -0,0 +1,20 @@
+import sys
+
+if __name__ == "__main__":
+    vcf_path = sys.argv[1]
+    length, n = 0, 0
+    with open(vcf_path, 'r') as VCF:
+        line = VCF.readline()
+
+        while line[:2] == "##":
+
+            if line[:9] == "##contig=": # line = "##contig=<ID=GL000224.1,assembly=b37,length=179693>\n"
+
+                for x in line[10:-1].split(","): # line[10:-1] = "ID=GL000224.1,assembly=b37,length=179693"  
+                    pair = x.split("=")
+
+                    if pair[0] == "length": # Necessary for invertion calculus
+                        length += int(pair[1])
+                        n += 1
+
+    print("Max pos over reference is arround", length / n, ".")
